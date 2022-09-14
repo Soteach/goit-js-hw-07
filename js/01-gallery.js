f4d552e23d91a76bf6bff325f941948df9c70c2d
Sergij Suprunenko <soteach1982@gmail.com>
2022-09-14 19:16:05 +0300

    make hw (first pass)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) =>
+        `<div class = "gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`)
+        .join("");
+}
+
+
+const addGalleryMarkup = createGallery(galleryItems);
+
+galleryBox.innerHTML = addGalleryMarkup;
+
+galleryBox.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+    // Забороняємо браузеру відкривати картинку за посиланням
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+// інакше виконуємо ligthbox
+    
+    const instance = basicLightbox.create(`<div class="modal"><img src="${event.target.dataset.source}" width="800" heigth = "600"></div>`);
+    instance.show();
+
+    galleryBox.addEventListener("keydown", (event) => {
+    
+    if (event.code === "Escape") {
+        instance.close();
+    }
+})
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryBox = document.querySelector(".gallery");
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) =>
+        `<div class = "gallery__item">
+        <a class="gallery__item" 
+        href="${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>
+</div>`)
+        .join("");
+}
+
+
+
+const addGalleryMarkup = createGallery(galleryItems);
+
+galleryBox.innerHTML = addGalleryMarkup;
+
+galleryBox.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+    // Забороняємо браузеру відкривати картинку за посиланням
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    console.log(event.target.alt);
+// інакше виконуємо ligthbox
+    let gallery = new SimpleLightbox('.gallery a', { captions: true, captionPosition: 'bottom',captionDelay:250, captionsData:"alt"});
+gallery.on('show.simplelightbox');
+    
+    galleryBox.addEventListener("keydown", (event) => {
+    
+    if (event.code === "Escape") {
+        instance.close();
+    }
+})
+}
